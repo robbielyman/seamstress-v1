fa7ad31161543da07245f6f824a25720c38c25aa
Rylee Lyman <rylee.alanza@gmail.com>
2023-07-27 23:34:50 -0400

    fix: ditch macos-aarch64 for the mome, attempt to fix linux build

--- ci/ubuntu.sh
+++ ci/ubuntu.sh
@@ -6,10 +6,10 @@ tar -xf zig-linux-x86_64-0.11.0-dev.3971+6bc9c4f71.tar.xz -C /tmp
 cp -R /tmp/zig-linux-x86_64-0.11.0-dev.3971+6bc9c4f71/* /opt/zig
 sudo apt-get -qq update && sudo apt-get -qy install \
                                 libncurses-dev libasound2-dev libfreetype-dev \
-                                libharfbuzz-dev libudev-dev lib-samplerate0-dev \
+                                libharfbuzz-dev libudev-dev libsamplerate0-dev \
                                 libdbus-1-dev libwayland-dev libxkbcommon-dev \
                                 libdecor-0-dev libxcb-xkb-dev libx11-dev \
-                                libegl-dev libxcursor-dev libext-dev libxi-dev \
+                                libegl-dev libxcursor-dev libxext-dev libxi-dev \
                                 libxrandr-dev libxss-dev libjack-dev libpipewire-0.3-dev \
                                 multimedia-devel libgudev-1.0-0 
 pushd lib/ziglua
