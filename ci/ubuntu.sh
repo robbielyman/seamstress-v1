a2088f7fbdcb7e215c7e4bd68a37e0b585218afd
Rylee Lyman <rylee.alanza@gmail.com>
2023-07-27 23:45:03 -0400

    fix: don't install a millionty packages, except do install libdrm

--- ci/ubuntu.sh
+++ ci/ubuntu.sh
@@ -4,14 +4,14 @@ mkdir /opt/zig
 wget https://ziglang.org/builds/zig-linux-x86_64-0.11.0-dev.3971+6bc9c4f71.tar.xz
 tar -xf zig-linux-x86_64-0.11.0-dev.3971+6bc9c4f71.tar.xz -C /tmp
 cp -R /tmp/zig-linux-x86_64-0.11.0-dev.3971+6bc9c4f71/* /opt/zig
-sudo apt-get -qq update && sudo apt-get -qy install \
-                                libncurses-dev libasound2-dev libfreetype-dev \
-                                libharfbuzz-dev libudev-dev libsamplerate0-dev \
-                                libdbus-1-dev libwayland-dev libxkbcommon-dev \
-                                libdecor-0-dev libxcb-xkb-dev libx11-dev \
-                                libegl-dev libxcursor-dev libxext-dev libxi-dev \
-                                libxrandr-dev libxss-dev libjack-dev libpipewire-0.3-dev \
-                                multimedia-devel libgudev-1.0-0 
+sudo apt-get install \
+     libncurses-dev libasound2-dev libfreetype-dev \
+     libharfbuzz-dev libudev-dev libsamplerate0-dev \
+     libdbus-1-dev libwayland-dev libxkbcommon-dev \
+     libdecor-0-dev libxcb-xkb-dev libx11-dev \
+     libegl-dev libxcursor-dev libxext-dev libxi-dev \
+     libxrandr-dev libxss-dev libjack-dev libpipewire-0.3-dev \
+     multimedia-devel libgudev-1.0-0 libdrm-dev
 pushd lib/ziglua
 /opt/zig/zig build -Doptimize=ReleaseFast
 popd
