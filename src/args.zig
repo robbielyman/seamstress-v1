fad0f6307ed2ba8e9e66913d6fcc64702aa36811
Rylee Lyman <rylee.alanza@gmail.com>
2023-08-12 12:16:24 -0400

    fix: allow `.lua` suffixes for script; don't require `-s`
    closes (#41)

--- src/args.zig
+++ src/args.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-pub var script_file: [:0]const u8 = "script";
+pub var script_file: []const u8 = "script";
 pub var local_port: [:0]const u8 = "7777";
 pub var remote_port: [:0]const u8 = "6666";
 pub var width: [:0]const u8 = "256";
@@ -8,6 +8,7 @@ pub var height: [:0]const u8 = "128";
 pub var watch = false;
 
 pub fn parse() !void {
+    var double_dip = false;
     var args = std.process.args();
     var i: u8 = 0;
     while (args.next()) |*arg| : (i += 1) {
@@ -15,7 +16,11 @@ pub fn parse() !void {
             continue;
         }
         if ((arg.len != 2) or (arg.*[0] != '-')) {
-            break;
+            if (!double_dip) {
+                script_file = arg.*;
+                double_dip = true;
+                continue;
+            } else break;
         }
         switch (arg.*[1]) {
             'b' => {
@@ -59,6 +64,13 @@ pub fn parse() !void {
         break;
     } else {
         args.deinit();
+        const suffix = ".lua";
+        if (script_file.len >= suffix.len and
+            std.mem.eql(
+            u8,
+            suffix,
+            script_file[(script_file.len - suffix.len)..script_file.len],
+        )) script_file = script_file[0..(script_file.len - suffix.len)];
         return;
     }
     args.deinit();
@@ -70,11 +82,14 @@ fn print_usage() !void {
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
-    try stdout.print("Start seamstress with optional overrides:\n", .{});
-    try stdout.print("-s    override user script [current {s}]\n", .{script_file});
-    try stdout.print("-l    override OSC listen port [current {s}]\n", .{local_port});
-    try stdout.print("-b    override OSC broadcast port [current {s}]\n", .{remote_port});
-    try stdout.print("-x    override window width [current {s}]\n", .{width});
-    try stdout.print("-y    override window height [current {s}]\n", .{height});
+    try stdout.print("USAGE: seamstress [script] [args]\n\n", .{});
+    try stdout.print("[script] (optional) should be the name of a lua file in CWD or ~/seamstress\n", .{});
+    try stdout.print("[args]   (optional) should be one of the following\n", .{});
+    try stdout.print("-s       override user script [current {s}]\n", .{script_file});
+    try stdout.print("-l       override OSC listen port [current {s}]\n", .{local_port});
+    try stdout.print("-b       override OSC broadcast port [current {s}]\n", .{remote_port});
+    try stdout.print("-w       watch the directory containing the script file for changes\n", .{});
+    try stdout.print("-x       override window width [current {s}]\n", .{width});
+    try stdout.print("-y       override window height [current {s}]\n", .{height});
     try bw.flush();
 }
