39a11032d769c3ba306353e6715f9a5a7ce5a349
Rylee Alanza Lyman <46907231+ryleelyman@users.noreply.github.com>
2023-08-30 13:45:55 -0400

    chore: bump version

--- src/main.zig
+++ src/main.zig
@@ -12,7 +12,7 @@ const midi = @import("midi.zig");
 const socket = @import("socket.zig");
 const watcher = @import("watcher.zig");
 
-const VERSION = .{ .major = 0, .minor = 18, .patch = 0 };
+const VERSION = .{ .major = 0, .minor = 19, .patch = 0 };
 
 pub const std_options = struct {
     pub const log_level = .info;
