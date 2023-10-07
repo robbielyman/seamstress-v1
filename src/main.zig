109d79365034d54801dc4ab6ac6bf72431f9b696
Rylee Lyman <rylee.alanza@gmail.com>
2023-10-07 11:55:59 -0700

    chore: bump version

--- src/main.zig
+++ src/main.zig
@@ -14,7 +14,7 @@ const watcher = @import("watcher.zig");
 const create = @import("create.zig");
 const pthread = @import("pthread.zig");
 
-const VERSION = .{ .major = 0, .minor = 24, .patch = 6 };
+const VERSION = .{ .major = 0, .minor = 25, .patch = 0 };
 
 pub const std_options = struct {
     pub const log_level = .info;
